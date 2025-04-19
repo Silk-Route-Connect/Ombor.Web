a764bba2cdac5067dba86fdb30c8f2dc0a8f7a92
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-04-19 14:07:51 +0500

    initial setup

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './styles/global.scss';
+import reportWebVitals from './reportWebVitals';
+
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
