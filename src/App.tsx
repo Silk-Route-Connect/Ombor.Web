25e36f8c170ab2c269f9fd37c0a721b4fc43ba7a
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-05-15 22:09:48 +0500

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AppLayout from './layouts/AppLayout';
-import CategoryPage from './pages/CategoryPage';
-import ProductPage from './pages/ProductPage';
-import { SnackbarProvider } from 'notistack';
-import { JSX, useEffect } from 'react';
-import { seedData } from './seedData';
-import { useStore } from './stores/StoreContext';
+import React, { JSX } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SnackbarProvider, useSnackbar } from "notistack";
+
+import AppLayout from "./layouts/AppLayout";
+import CategoryPage from "./pages/CategoryPage";
+import ProductPage from "./pages/ProductPage";
+import { rootStore } from "./stores/RootStore";
+import { StoreProvider } from "./stores/StoreContext";
+
+function SnackbarInjector({ children }: { children: React.ReactNode }) {
+	const { enqueueSnackbar } = useSnackbar();
+	React.useEffect(() => {
+		rootStore.notificationStore.inject(enqueueSnackbar);
+	}, [enqueueSnackbar]);
+	return <>{children}</>;
+}
 
 function App(): JSX.Element {
-  const { categoryStore, productStore } = useStore();
-
-  useEffect(() => {
-    // seed fake data for both categories and products
-    seedData();
-
-    // if you still want to load real/mock-API data afterwards:
-    // categoryStore.loadCategories();
-    // productStore.loadProducts();
-  }, [categoryStore, productStore]);
-
-  return (
-    <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<CategoryPage />} />
-            <Route path="categories" element={<CategoryPage />} />
-            <Route path="products" element={<ProductPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </SnackbarProvider>
-  );
+	return (
+		<SnackbarProvider
+			maxSnack={3}
+			anchorOrigin={{
+				vertical: "top",
+				horizontal: "center",
+			}}
+		>
+			<SnackbarInjector>
+				<StoreProvider>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<AppLayout />}>
+								<Route index element={<CategoryPage />} />
+								<Route path="categories" element={<CategoryPage />} />
+								<Route path="products" element={<ProductPage />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</StoreProvider>
+			</SnackbarInjector>
+		</SnackbarProvider>
+	);
 }
 
 export default App;
-
