387435dcd8ce88e6578726a0c28c20c6be89ae35
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-05-15 21:45:27 +0500

    improve linters and formatters

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	root: true,
+	env: {
+	  browser: true,
+	  node: true,
+	  es2021: true
+	},
+	ignorePatterns: ["node_modules/", ".eslintrc.js"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+	  ecmaVersion: 2021,
+	  sourceType: "module",
+	  ecmaFeatures: { jsx: true }
+	},
+	plugins: [
+	  "react",
+	  "react-hooks",
+	  "@typescript-eslint",
+	  "prettier",
+	  "simple-import-sort",
+	  "sonarjs"
+	],
+	extends: [
+	  "eslint:recommended",
+	  "plugin:react/recommended",
+	  "plugin:@typescript-eslint/recommended",
+	  "plugin:prettier/recommended"
+	],
+	rules: {
+	  "react/react-in-jsx-scope": "off",
+	  "indent": "off",
+	  "prettier/prettier": "error",
+	  "quotes": ["error", "double", { avoidEscape: true }],
+	  "jsx-quotes": ["error", "prefer-double"],
+	  "semi": ["error", "always"],
+	  "comma-dangle": ["error", "always-multiline"],
+	  "max-len": ["warn", { code: 100, ignoreUrls: true, ignoreStrings: true, ignoreComments: true }],
+	  "simple-import-sort/imports": ["error", {
+		groups: [
+		  ["^react", "^@?\\w"],
+		  ["^src/"],
+		  ["^\\u0000"],
+		  ["^\\.\\.(?!/?$)", "^\\.\\./?$", "^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"]
+		]
+	  }],
+	  "sonarjs/no-duplicate-string": "warn",
+	  "sonarjs/cognitive-complexity": ["warn", 20]
+	},
+	settings: {
+	  react: { version: "detect" }
+	}
+  };
+  
