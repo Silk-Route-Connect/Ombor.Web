6c4068a5c8172938d3cab3fadbd600d8b5d20bc5
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-05-24 14:41:46 +0500

    setup sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import * as Sentry from "@sentry/react";
 
 import "./index.css";
 import "./styles/global.scss";
@@ -9,6 +10,13 @@ import "./styles/global.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
+
+Sentry.init({
+	dsn: "https://84bb737c6cfcd8d390525efba2411f35@o4508670634557440.ingest.de.sentry.io/4509377451655248",
+	// Setting this option to true will send default PII data to Sentry.
+	// For example, automatic IP address collection on events
+	sendDefaultPii: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
