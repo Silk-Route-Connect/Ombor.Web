37ddd8d1cb8b53f3bf59ceac9a3c8f3273b6d815
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-06-03 23:21:13 +0500

    add supplier models and API

---
+++ src/models/supplier.ts
@@ -0,0 +1,19 @@
+export type Supplier = {
+	id: number;
+	name: string;
+	address?: string;
+	email?: string;
+	phoneNumbers: string[];
+	companyName?: string;
+	isActive: boolean;
+};
+
+export type CreateSupplierRequest = Omit<Supplier, "id">;
+
+export type UpdateSupplierRequest = CreateSupplierRequest & {
+	id: number;
+};
+
+export type GetSuppliersRequest = {
+	searchTerm?: string;
+};
