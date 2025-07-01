c6febb07e8fed76fd1237edc1b58aca20e13221b
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-01 21:13:48 +0500

    improve numeric input

--- src/components/shared/Inputs/NumericField.tsx
+++ src/components/shared/Inputs/NumericField.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 
-export type NumberFieldProps = TextFieldProps & {
+export type NumericField = TextFieldProps & {
 	selectOnFocus?: boolean;
 };
 
-const NumericField: React.FC<NumberFieldProps> = ({ selectOnFocus = true, slotProps, ...rest }) => {
+const NumericField: React.FC<NumericField> = ({
+	selectOnFocus = true,
+	fullWidth = true,
+	slotProps = {},
+	onChange,
+	...rest
+}) => {
 	return (
 		<TextField
 			{...rest}
 			type="number"
-			fullWidth
+			fullWidth={fullWidth}
+			onChange={(e) => {
+				if (!onChange) {
+					return;
+				}
+
+				const value = e.target.value;
+				if (value.length > 1 && value.startsWith("0")) {
+					e.target.value = value.replace(/^0+/, "");
+				}
+
+				onChange(e);
+			}}
 			slotProps={{
 				input: {
 					inputMode: "decimal",
