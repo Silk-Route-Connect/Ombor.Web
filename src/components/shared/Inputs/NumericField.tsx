345113d830c4b038418da88febb8175600a36a4c
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-16 23:19:14 +0500

    disable up/down arrows on numeric field

--- src/components/shared/Inputs/NumericField.tsx
+++ src/components/shared/Inputs/NumericField.tsx
@@ -1,48 +1,50 @@
 import React from "react";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 
-export type NumericField = TextFieldProps & {
+export type NumericFieldProps = TextFieldProps & {
 	selectOnFocus?: boolean;
 };
 
-const NumericField: React.FC<NumericField> = ({
+const NumericField: React.FC<NumericFieldProps> = ({
 	selectOnFocus = true,
 	fullWidth = true,
 	slotProps = {},
 	onChange,
 	...rest
-}) => {
-	return (
-		<TextField
-			{...rest}
-			type="number"
-			fullWidth={fullWidth}
-			onChange={(e) => {
-				if (!onChange) {
-					return;
-				}
+}) => (
+	<TextField
+		{...rest}
+		sx={{
+			"& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": {
+				display: "none",
+			},
+			"& input[type=number]": {
+				MozAppearance: "textfield",
+			},
+		}}
+		type="number"
+		fullWidth={fullWidth}
+		onChange={(e) => {
+			if (!onChange) return;
 
-				const value = e.target.value;
-				if (value.length > 1 && value.startsWith("0")) {
-					e.target.value = value.replace(/^0+/, "");
-				}
-
-				onChange(e);
-			}}
-			slotProps={{
-				input: {
-					inputMode: "decimal",
-					...(selectOnFocus && {
-						onFocus: (e: React.FocusEvent<HTMLInputElement>) => {
-							e.target.select();
-						},
-					}),
-					...(slotProps?.input ?? {}),
-				},
-				...slotProps,
-			}}
-		/>
-	);
-};
+			/* strip leading zeroes */
+			const v = e.target.value;
+			if (v.length > 1 && v.startsWith("0")) {
+				e.target.value = v.replace(/^0+/, "");
+			}
+			onChange(e);
+		}}
+		slotProps={{
+			input: {
+				inputMode: "decimal",
+				...(selectOnFocus && {
+					onFocus: (e: React.FocusEvent<HTMLInputElement>) => e.target.select(),
+				}),
+				...(slotProps?.input ?? {}),
+			},
+			...slotProps,
+		}}
+	/>
+);
 
 export default NumericField;
