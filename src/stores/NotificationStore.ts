dc478a1f3719d32de20c299e48159a05007c29e4
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-31 16:08:15 +0500

    update notification store

--- src/stores/NotificationStore.ts
+++ src/stores/NotificationStore.ts
@@ -1,22 +1,40 @@
 import { makeAutoObservable } from "mobx";
-import type { OptionsObject, ProviderContext } from "notistack";
+import type {
+	EnqueueSnackbar,
+	OptionsObject,
+	ProviderContext,
+	SnackbarKey,
+	SnackbarMessage,
+} from "notistack";
+
+type PendingToast = Parameters<EnqueueSnackbar>;
 
 export class NotificationStore {
-	private enqueue!: ProviderContext["enqueueSnackbar"];
+	private pending: PendingToast[] = [];
+	private enqueue: EnqueueSnackbar = (
+		message: SnackbarMessage,
+		options?: OptionsObject,
+	): SnackbarKey => {
+		this.pending.push([message, options]);
+		return "" as SnackbarKey;
+	};
 
 	constructor() {
-		makeAutoObservable(this);
+		makeAutoObservable(this, {}, { autoBind: true });
 	}
 
-	inject(enqueueSnackbar: ProviderContext["enqueueSnackbar"]) {
-		this.enqueue = enqueueSnackbar;
+	error(message: string, opts?: OptionsObject) {
+		this.enqueue(message, { variant: "error", ...opts });
 	}
 
-	error(msg: string, opts?: OptionsObject) {
-		this.enqueue(msg, { variant: "error", ...opts });
+	success(message: string, opts?: OptionsObject) {
+		this.enqueue(message, { variant: "success", ...opts });
 	}
 
-	success(msg: string, opts?: OptionsObject) {
-		this.enqueue(msg, { variant: "success", ...opts });
+	inject(realEnqueue: ProviderContext["enqueueSnackbar"]) {
+		this.enqueue = realEnqueue;
+
+		this.pending.forEach(([msg, opts]) => realEnqueue(msg, opts));
+		this.pending = [];
 	}
 }
