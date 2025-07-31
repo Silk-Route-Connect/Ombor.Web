07669e4ca8c346ec2f1fa02d55f668b616d00c16
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-07-31 16:34:02 +0500

    remove redundant space

--- src/components/category/Header/CategoryHeader.tsx
+++ src/components/category/Header/CategoryHeader.tsx
@@ -5,36 +5,40 @@ import { PrimaryButton } from "components/shared/PrimaryButton/PrimaryButton";
 import { SearchInput } from "components/shared/SearchInput/SearchInput";
 import { translate } from "i18n/i18n";
 
-export interface IProps {
+interface CategoryHeaderProps {
 	title?: string;
 	searchValue: string;
+
 	onCreate: () => void;
 	onSearch: (value: string) => void;
 }
 
-const CategoryHeader: React.FC<IProps> = ({
+const CategoryHeader: React.FC<CategoryHeaderProps> = ({
 	title,
 	searchValue,
 	onCreate,
 	onSearch,
-}): JSX.Element => {
-	return (
-		<Box display="flex" justifyContent="space-between" alignItems="center" mb={3} flexWrap="wrap">
-			<Typography variant="h5">{title}</Typography>
-			<Box display="flex" alignItems="center" mt={{ xs: 2, sm: 0 }}>
-				<SearchInput
-					value={searchValue}
-					onChange={onSearch}
-					placeholder={translate("category.searchTitle")}
-				/>
-				<Box ml={2}>
-					<PrimaryButton icon={<AddIcon />} onClick={onCreate}>
-						{translate("common.create")}
-					</PrimaryButton>
-				</Box>
-			</Box>
+}): JSX.Element => (
+	<Box
+		display="flex"
+		flexWrap="wrap"
+		justifyContent="space-between"
+		alignItems="center"
+		mb={3}
+		sx={{ gap: 2 }}
+	>
+		<Typography variant="h5">{title}</Typography>
+		<Box display="flex" alignItems="center" sx={{ gap: 2 }}>
+			<SearchInput
+				value={searchValue}
+				onChange={onSearch}
+				placeholder={translate("category.searchTitle")}
+			/>
+			<PrimaryButton icon={<AddIcon />} onClick={onCreate}>
+				{translate("common.create")}
+			</PrimaryButton>
 		</Box>
-	);
-};
+	</Box>
+);
 
 export default CategoryHeader;
