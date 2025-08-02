8dca29df6c4d42f2c6503d48cb8b63c8f21eb2d7
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-08-02 11:19:44 +0500

    add partner & transaction links

---
+++ src/components/shared/Links/TransactionLink.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "@mui/material";
+import { translate } from "i18n/i18n";
+import { TransactionType } from "models/transaction";
+import { TRANSACTION_ROUTES } from "routing";
+
+interface TransactionLinkProps {
+	id: number;
+	type: TransactionType;
+}
+
+const TransactionLink: React.FC<TransactionLinkProps> = ({ id, type }) => {
+	const path = `${TRANSACTION_ROUTES[type]}/${id}`;
+	const typeLabel = translate(`transaction.type.${type}`);
+
+	return (
+		<Link
+			href={path}
+			underline="none"
+			sx={{ color: "primary.main", "&:hover": { textDecoration: "underline" } }}
+		>
+			{`${typeLabel} #${id}`}
+		</Link>
+	);
+};
+
+export default TransactionLink;
