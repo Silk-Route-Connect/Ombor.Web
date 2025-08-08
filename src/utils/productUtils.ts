60b7c952185a96464bc6bc9a5faa57f8004c8aab
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-08-08 21:19:54 +0500

    update product & add template utility methods

--- src/utils/productUtils.ts
+++ src/utils/productUtils.ts
@@ -3,3 +3,14 @@ import { Product } from "models/product";
 export function isAvialableForSale(product: Product) {
 	return product.quantityInStock >= 0;
 }
+
+export function calculateLineTotals(unitPrice: number, quantity: number, discount: number) {
+	const lineTotal = unitPrice * quantity;
+	const discountAmount = (lineTotal * discount) / 100;
+
+	return { lineTotal, discountAmount };
+}
+
+export function getPrice(product: Product, type: "Sale" | "Supply") {
+	return type === "Sale" ? product.salePrice : product.supplyPrice;
+}
