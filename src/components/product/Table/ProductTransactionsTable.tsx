26a38f163a3d46316d58fd6a6c9c12c78f8fb810
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-08-24 21:28:05 +0500

    implement product transactions table

---
+++ src/components/product/Table/ProductTransactionsTable.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Column, DataTable, SortOrder } from "components/shared/DataTable/DataTable";
+import { Loadable } from "helpers/Loading";
+import { ProductTransaction } from "models/product";
+
+import {
+	buildProductTransactionsColumns,
+	ProductTransactionsMode,
+} from "./productTransactionsTableConfigs";
+
+export interface ProductTransactionsTableProps {
+	mode: ProductTransactionsMode;
+	data: Loadable<ProductTransaction[]>;
+	pagination: boolean;
+	onSort?: (field: keyof ProductTransaction, order: SortOrder) => void;
+}
+
+const ProductTransactionsTable: React.FC<ProductTransactionsTableProps> = ({
+	mode,
+	data,
+	pagination = true,
+	onSort,
+}) => {
+	const columns: Column<ProductTransaction>[] = buildProductTransactionsColumns(mode);
+
+	return (
+		<DataTable<ProductTransaction>
+			rows={data}
+			columns={columns}
+			pagination={pagination}
+			onSort={onSort}
+		/>
+	);
+};
+
+export default ProductTransactionsTable;
