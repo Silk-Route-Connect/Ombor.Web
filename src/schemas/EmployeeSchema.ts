9864190cffa4b30d0910acb6f694e999600e0b91
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-11-04 20:51:20 +0500

    improve email regex
    set max size limit

--- src/schemas/EmployeeSchema.ts
+++ src/schemas/EmployeeSchema.ts
@@ -15,7 +15,7 @@ export const PhoneNumberSchema = z
 		message: translate("employee.validation.phoneNumberInvalid"),
 	});
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const emailRegex = /^[^\s@]{1,64}@[^\s@]{1,255}\.[^\s@]{2,63}$/;
 
 const ContactInfoSchema = z
 	.object({
