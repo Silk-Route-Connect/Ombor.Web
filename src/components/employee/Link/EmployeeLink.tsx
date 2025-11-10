415ee005d0959489c90d4abd39657327c34d5813
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2025-11-11 01:31:20 +0500

    add employee link

---
+++ src/components/employee/Link/EmployeeLink.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Link } from "@mui/material";
+
+interface EmployeeLinkProps {
+	id: number;
+	name: string;
+}
+
+const EmployeeLink: React.FC<EmployeeLinkProps> = ({ id, name }) => (
+	<Link
+		href={`/partners/${id}`}
+		underline="none"
+		sx={{ color: "primary.main", "&:hover": { textDecoration: "underline" } }}
+	>
+		{name}
+	</Link>
+);
+
+export default EmployeeLink;
